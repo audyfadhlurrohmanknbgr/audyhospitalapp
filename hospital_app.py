54572d110735aa19d0a052f43c2e5c1e17096656
audyfadhlurrohmanknbgr <audy.fadhlurrohman@kodingnext.com>
2026-06-28 16:43:38 +0700

    Create hospital_app.py

---
+++ hospital_app.py
@@ -0,0 +1,8 @@
+import streamlit as at
+import pandas as pd
+import numpy as np
+import pickle
+import os
+
+st.set_page(page_title="Smart Hospital Patient Navigator", page_icon="",
+            layout="wide")
